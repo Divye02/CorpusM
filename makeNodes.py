11cd1cb7399c33460e5f9ec6959025957af76895
Divye Jain <dpjain@dirk.cs.washington.edu>
2015-11-10 14:08:57 -0800

    making relationships

--- makeNodes.py
+++ makeNodes.py
@@ -8,6 +8,12 @@ Keywords = [[],['nutrients']];
 
 for a,b in zip(Content,Keywords):
 	graph.create(Node("Content", Text = a, keywords = b));
+content = ['Animals take in air by breathing. They need oxygen,
+which is in the air. Oxygen allows the animal to make
+and use energy, which it needs to survive.', 'Animals also need water to survive. Water is used to break down and move materials throughout the body.', 'Animals cannot make their own food so they must eat to get nutrients. Nutrients are necessary for growth and energy.'];
+keywords = [[],[],['nutrients']];
+for a,b in zip(content, keywords)
+	graph.create(Relationship(Node("Topic1", name = "Animals"), "CONTAINS", Node("Content", Text=a, keywords=b);
 
 
   
